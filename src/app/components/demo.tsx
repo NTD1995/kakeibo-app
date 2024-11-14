04e7e2a453f25546c87774ca7e528d8ac8085e5f
Shogo Wada <aiz4338150@gmail.com>
2024-11-14 10:55:09 +0900

    Chakra UIのデモ用ボタン追加

---
+++ src/app/components/demo.tsx
@@ -0,0 +1,12 @@
+'use client';
+
+import { Button, HStack } from '@chakra-ui/react';
+
+export const Demo = () => {
+  return (
+    <HStack>
+      <Button colorScheme="blue">Button</Button>
+      <Button colorScheme="green">Button</Button>
+    </HStack>
+  );
+};
